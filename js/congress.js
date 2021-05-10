d0f6ecccab7b9cf874c5c15df778088d4660e46e
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2021-05-10 09:07:01 -0500

    Changing slideshow to change automatically

--- js/congress.js
+++ js/congress.js
@@ -1,11 +1,33 @@
-var slideIndex = 1;
-
+//var slideIndex = 1;
+/*
 window.onload = function() {
   showSlides(slideIndex);
 };
 
 showSlides(slideIndex);
+*/
 
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo cursor");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+/*
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -57,4 +79,4 @@ document.getElementById("photo5").addEventListener('click', function(){
 document.getElementById("photo6").addEventListener('click', function(){
     currentSlide(6);
 });
-
+*/
