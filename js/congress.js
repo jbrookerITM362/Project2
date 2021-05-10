4005ce37ae2a3fe9ad246e2bb581963721cd95ea
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2021-05-10 09:02:24 -0500

    Update congress.js

--- js/congress.js
+++ js/congress.js
@@ -17,7 +17,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
+  var dots = document.getElementsByClassName("demo cursor");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
